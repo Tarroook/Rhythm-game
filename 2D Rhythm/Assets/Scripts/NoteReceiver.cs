c6fc5cf95394a6e0ba5e3e80e49bf9b26f48bcff
Tarook <Tarook.Zinou@gmail.com>
2022-09-06 22:15:24 +0200

    signals now launch unity events

--- 2D Rhythm/Assets/Scripts/NoteReceiver.cs
+++ 2D Rhythm/Assets/Scripts/NoteReceiver.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.Playables;
 
 public class NoteReceiver : MonoBehaviour, INotificationReceiver
 {
+
+    public UnityEvent onWindUpSignal;
+
+    private void Awake()
+    {
+        if (onWindUpSignal == null)
+            onWindUpSignal = new UnityEvent();
+    }
     public void OnNotify(Playable origin, INotification notification, object context)
     {
-        if(notification is NoteMarker noteMarker) // do this so it doesn't react to all signals
+        if(notification is EnemyWindUpSignal sig) // do this so it doesn't react to all signals
         {
-            Debug.Log(notification);
+            onWindUpSignal.Invoke();
         }
     }
 }
