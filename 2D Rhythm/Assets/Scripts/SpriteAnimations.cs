1d9abebc8230d7a082bf685cdbe9eb0ddf91f2cd
Tarook <Tarook.Zinou@gmail.com>
2022-09-23 22:16:59 +0200

    Plays animations but whacky
    Need to make them slower + not return immediately

--- 2D Rhythm/Assets/Scripts/SpriteAnimations.cs
+++ 2D Rhythm/Assets/Scripts/SpriteAnimations.cs
@@ -4,24 +4,45 @@ using UnityEngine;
 
 public class SpriteAnimations : MonoBehaviour
 {
-
     private Animator anim;
 
     private void OnEnable()
     {
         MusicManager.onBeat += beatAnimation;
+        InputManager.onPressedAttack += attackAnimation;
+        InputManager.onPressedDodge += dodgeAnimation;
     }
     private void OnDisable()
     {
         MusicManager.onBeat -= beatAnimation;
+        InputManager.onPressedAttack -= attackAnimation;
     }
 
     private void Start()
     {
         anim = GetComponent<Animator>();
     }
-    public void beatAnimation()
+
+    private void beatAnimation()
     {
         anim.Play("Bounce Layer.Player_Bounce", 1);
+    }
+
+    private void attackAnimation()
+    {
+        
+    }
+
+    private void dodgeAnimation(string direction)
+    {
+        switch (direction)
+        {
+            case "left":
+                anim.SetTrigger("SlideLeft");
+                break;
+            case "right":
+                anim.SetTrigger("SlideRight");
+                break;
+        }
     }
 }
