55bf830e2e3b30f388f30ec37e71292c89e667f9
Tarook <Tarook.Zinou@gmail.com>
2022-10-15 16:45:34 +0200

    Added "level select" which doesn't do anything yet

--- 2D Rhythm/Assets/MainMenu.cs
+++ 2D Rhythm/Assets/MainMenu.cs
@@ -5,9 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+
+    private Camera mainCam;
+
+    private void Awake()
+    {
+        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+    }
+
     public void playPress()
     {
-        SceneManager.LoadScene("Level");
+        //SceneManager.LoadScene("Level");
+        mainCam.GetComponent<Animator>().SetTrigger("Play");
     }
 
     public void quitGame()
